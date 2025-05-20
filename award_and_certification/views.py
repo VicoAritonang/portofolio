a80fde858599e847c76ffa600b5edf2b3e932b31
Vico Winner Sebastian Aritonang <chaikaaaaa5@gmail.com>
2025-05-20 14:08:45 +0700

    Update views.py

--- award_and_certification/views.py
+++ award_and_certification/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from supabase_client import supabase
 
 # Create your views here.
-
+# nothing to do here
 def award_list(request):
     """
     View for listing all awards and certifications
@@ -10,5 +10,4 @@ def award_list(request):
     # Fetch all papers from Supabase
     response = supabase.table("paper").select("*").order("date", desc=True).execute()
     papers = response.data if response.data else []
-    papers.date = datetime.strptime(paper.date, '%Y-%m-%d').date()
     return render(request, 'award_and_certification/award_list.html', {'papers': papers})
